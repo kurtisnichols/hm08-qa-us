a19f31565645a8152346908041748d0935d9d270
Kurtis Nichols <kurtis.nichols@yahoo.com>
2024-10-23 12:03:19 -0700

    added code to helper

--- helper.js
+++ helper.js
@@ -1,9 +1,15 @@
 module.exports = {
-    getPhoneNumber: function(countryCode) {
-        const number = Math.floor(1000000000 + Math.random() * 9000000000)
-        return `${countryCode}${number}`
-    },
-    getElementByText: async function(obj) {
-        return await $(`div=${obj.toString()}`);
-    }
+  getPhoneNumber: function (countryCode) {
+    const number = Math.floor(1000000000 + Math.random() * 9000000000);
+    return `${countryCode}${number}`;
+  },
+
+  getElementByText: async function (text, tag = "div") {
+    return await $(`${tag}=${text}`);
+  },
+
+  // Function to simulate pressing TAB
+  pressTab: async function () {
+    await browser.keys("Tab");
+  },
 };
