07e00e2d9badb0ac5178ec04c403cce12cabbbe8
Kurtis Nichols <kurtis.nichols@yahoo.com>
2024-10-31 11:09:54 -0700

    updated expect for tests

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -32,25 +32,60 @@ describe("Create an order", () => {
   it("should select a supportive plan", async () => {
     await browser.url("/");
     await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
-    await page.selectSupportivePlan();
+    const supportiveButton = $('div=Supportive');
+    await supportiveButton.click();
+    await expect(supportiveButton).toBeExisting();
   });
 
   it("should order blanket and handkerchiefs", async () => {
-    await browser.url("/");
+    await browser.url(`/`);
     await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+
     await page.orderBlanketAndHandkerchiefs();
+
+    const blanketSwitchLabel = await $(
+      "div.r-sw-label=Blanket and handkerchiefs"
+    );
+    const blanketSwitchInput = await blanketSwitchLabel
+      .$("..")
+      .$("input.switch-input");
+    await expect(blanketSwitchInput).toBeChecked();
   });
 
   it("should order 2 ice creams", async () => {
     await browser.url("/");
     await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
-    await page.orderIceCreams(2);
+    const iceCreamCounterPlusButton = await $(page.iceCreamCounterPlusButton);
+    await iceCreamCounterPlusButton.waitForDisplayed();
+    await iceCreamCounterPlusButton.click();
+    await iceCreamCounterPlusButton.click();
+    const iceCreamValue = 2;
+    await expect($(`div=${iceCreamValue}`)).toBeExisting();
   });
 
-  it("should select a payment method", async () => {
-    await browser.url("/");
+  it("should add a credit card", async () => {
+    await browser.url(`/`);
     await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
-    await page.selectPaymentMethod();
+    const paymentMethod = await $(page.paymentMethod);
+    await paymentMethod.click();
+    const addCardButton = await $(page.addCardButton);
+    await addCardButton.click();
+    const cardNumberField = await $(page.cardNumberField);
+    await cardNumberField.waitForDisplayed();
+    await cardNumberField.click();
+    await cardNumberField.setValue("1234 5678 9000 1111");
+    const cardCodeField = await $(page.cardCodeField);
+    await cardCodeField.waitForDisplayed();
+    await cardCodeField.click();
+    await cardCodeField.setValue("123");
+    const linkCardButton = await $(page.linkCardButton);
+    await linkCardButton.waitForDisplayed();
+    await linkCardButton.click();
+    const cardModalCloseButton = await $(page.cardModalCloseButton);
+    await cardModalCloseButton.waitForDisplayed();
+    await cardModalCloseButton.click();
+    const addedCard = await $(page.addedCard);
+    await expect(addedCard).toBeExisting();
   });
 
   it("should display car search modal", async () => {
@@ -64,7 +99,7 @@ describe("Create an order", () => {
     await page.writeDriverMessage(message);
     await page.clickPlaceOrderButton();
 
-    const carSearchModal = await $('div=Car search'); // Use the updated selector for the car search modal
+    const carSearchModal = await $('div=Car search'); 
     await carSearchModal.waitForDisplayed({ timeout: 45000 });
     await expect(carSearchModal).toHaveText("Car search");
   });
